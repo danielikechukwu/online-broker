8a9ef28f9fcb4f4b2d7036a6c972d34b5be9445d
Daniel ikechukwu <ikechukwudaniel080@gmail.com>
2025-09-09 23:34:27 +0100

    Initial commit and project setup

---
+++ src/app/features/dashboard/dashboard.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [MatButtonModule],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.scss'
+})
+export class Dashboard {
+
+}
